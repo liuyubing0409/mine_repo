11ef8ba5a427d2f05a01d2fb6c22322b9791a28b
liuyubing <1477039470@qq.com>
2019-10-28 17:48:57 +0800

    Update class Test1

--- testaa/src/com/liuyb/test/Test1.java
+++ testaa/src/com/liuyb/test/Test1.java
@@ -3,7 +3,7 @@ package com.liuyb.test;
 public class Test1 {
 
 	public static void main(String[] args) {
-		System.out.println("=======TestA.java======");
+		System.out.println("===21==q==TestA.java===32==t=");
 	}
 
 }
